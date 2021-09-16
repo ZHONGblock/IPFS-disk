ba86e155d1240609bb609fdb0d31d4bcf5f5955c
ZHONGblock <75060727+ZHONGblock@users.noreply.github.com>
2021-09-16 16:25:47 +0800

    Add files via upload

--- models/ipfs/ipfs.go
+++ models/ipfs/ipfs.go
@@ -1,1 +1,17 @@
+package ipfs
 
+import (
+	"io"
+
+	shell "github.com/ipfs/go-ipfs-api"
+)
+
+func AddFile(r io.Reader, options ...shell.AddOpts) (string, error) {
+	sh := shell.NewShell("localhost:ipfsport")
+	cid, err := sh.Add(r)
+	if err != nil {
+		return "", err
+	}
+
+	return cid, err
+}
